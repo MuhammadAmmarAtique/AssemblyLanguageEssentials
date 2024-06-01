628fe1478073a54345b4797b4a89f0965dfb68aa
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-06-01 17:35:49 +0500

    added spacing

--- 03_Sum.asm
+++ 03_Sum.asm
@@ -5,20 +5,20 @@
 .CODE
 main proc
 
-mov al,1
-mov cl,2
+    mov al,1
+    mov cl,2
 
-add al,cl
-; it converts the numeric result of AL into its ASCII equivalent so that it can be displayed as a character.
-; 51 decimal & '3' character
-add al,48 
-mov dl,al
+    add al,cl
+    ; it converts the numeric result of AL into its ASCII equivalent so that it can be displayed as a character.
+    ; 51 decimal & '3' character
+    add al,48 
+    mov dl,al
 
-mov ah,2
-int 21H
+    mov ah,2
+    int 21H
 
-mov ah,4CH
-int 21H
+    mov ah,4CH
+    int 21H
 
 main endp
 end main
